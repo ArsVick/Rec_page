a3e2d2ddadfd35b5d9e2713dfdfb821c80960382
ArsVick <archigoy@gmail.com>
2021-10-24 23:50:02 +0300

    сделал сайт

---
+++ js/script.js
@@ -0,0 +1,34 @@
+/*function viewId(){
+    var first2 = document.getElementById("unique1").value.toString();
+};
+
+butt.onclick = function() {
+    var val = document.getElementById('elem1').value;
+    document.getElementById('str').innerHTML=val.toString();
+};*/
+
+function viewDiv(e){
+    e.preventDefault()
+    document.getElementById("recommend__book1__name").style.display = "block";
+    document.getElementById("recommend__book2__name").style.display = "block";
+    document.getElementById("recommend__book3__name").style.display = "block";
+    document.getElementById("recommend__book4__name").style.display = "block";
+    document.getElementById("recommend__book5__name").style.display = "block";
+    document.getElementById("text-area").style.display = "block";
+    /*viewId();*/
+    var first2 = document.getElementById("unique1").value.toString();
+    $.ajax({
+        url: "http://0.0.0.0:8080/lib/"+first2,
+        method: "GET",
+        success: function(resp) {console.log(resp)}
+})
+};
+
+
+
+/*
+$.ajax({
+    url: "http://0.0.0.0:8080/lib/" + first2,
+    method: "GET",
+    success: function(resp) {console.log(resp)}
+})*/
